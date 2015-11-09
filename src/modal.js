07908232f80802020a9ca902c1897cbf22fcac44
Andrew Levine <theandrewtizer@gmail.com>
2015-11-09 02:32:32 -0600

    Add keyboard shortcuts in modal

--- src/modal.js
+++ src/modal.js
@@ -1,23 +1,32 @@
 import Promise from 'bluebird';
 import {$, addClass, removeClass} from 'utils';
 
+const ENTER = 13;
+const ESC = 27;
+
 const titleEl = $('.modal-title');
 const messageEl = $('.modal-content');
 const modalBtn = $('.modal-ok');
 const toggleClasses = ['open-modal', 'overlay'];
+
+
+function keyboardCb(res, {keyCode:key}) {
+    if (key === ENTER || key === ESC) closeModal(res);
+}
+
+function closeModal(res) {
+    removeClass(document.body, toggleClasses);
+    document.body.removeEventListener('keydown', keyboardCb);
+    modalBtn.removeEventListener('click', closeModal);
+}
 
 export function open({title, message}) {
     titleEl.textContent = title;
     messageEl.textContent = message;
     addClass(document.body, toggleClasses);
 
-    let clickCb = (res) => {
-        removeClass(document.body, toggleClasses);
-        modalBtn.removeEventListener('click', clickCb);
-        res();
-    };
-
     return new Promise((res, rej) => {
-        modalBtn.addEventListener('click', () => clickCb(res));
+        modalBtn.addEventListener('click', () => closeModal(res));
+        document.body.addEventListener('keydown', e => keyboardCb(res, e));
     });
 }
