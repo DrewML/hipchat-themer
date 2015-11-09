9135f9ed707622986d7a3d3404f8a9f7ed5451d4
Andrew Levine <theandrewtizer@gmail.com>
2015-11-08 23:29:57 -0600

    Small clean-up of main.js, removing artifacts from electron sample project

--- main.js
+++ main.js
@@ -1,26 +1,15 @@
-var app = require('app'); // Module to control application life.
-var BrowserWindow = require('browser-window'); // Module to create native browser window.
+'use strict';
 
-// Report crashes to our server.
-require('crash-reporter').start();
+let app = require('app');
+let BrowserWindow = require('browser-window'); // Module to create native browser window.
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
+let mainWindow = null;
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
-});
+app.on('window-all-closed', app.quit.bind(app));
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', function() {
-    // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 550,
         height: 410,
@@ -30,17 +19,11 @@ app.on('ready', function() {
         'max-height': 410
     });
 
-    // and load the index.html of the app.
     mainWindow.loadUrl('file://' + __dirname + '/index-built.html');
 
-    // Open the DevTools.
-    mainWindow.openDevTools();
+    // mainWindow.openDevTools();
 
-    // Emitted when the window is closed.
     mainWindow.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         mainWindow = null;
     });
 });
